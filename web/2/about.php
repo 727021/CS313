09cc7aadab57eedc08462813f766973677b3670a
727021 <1834544+727021@users.noreply.github.com>
2019-09-28 16:35:05 -0600

    Add border to main content

--- web/2/about.php
+++ web/2/about.php
@@ -7,9 +7,12 @@
     <?php include('nav.php'); ?>
 
     <!-- Main page content -->
-    <main class="flex-shrink-0">
-        <div class="container">
-            <h1 class="display-4">About Me</h1>
+    <main class="flex-shrink-0 pl-1 pr-1 pl-md-0 pr-md-0">
+        <div class="container border rounded mt-1 mb-2">
+            <h1 class="display-4 border-bottom border-secondary">About Me</h1>
+            <div class="row">
+
+            </div>
         </div>
     </main>
 
--- web/2/index.php
+++ web/2/index.php
@@ -7,9 +7,9 @@
     <?php include('nav.php'); ?>
 
     <!-- Main page content -->
-    <main class="flex-shrink-0">
-        <div class="container">
-            <h1 class="display-4">Home</h1>
+    <main class="flex-shrink-0 pl-1 pr-1 pl-md-0 pr-md-0">
+        <div class="container border rounded mt-1 mb-2">
+            <h1 class="display-4 border-bottom border-secondary">Home</h1>
             <div class="row">
                 <div class="col-12 col-md-4">
                     <div class="box">
