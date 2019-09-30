4b4d3b9b6437062736a8b4e85f7b116966e6932a
727021 <1834544+727021@users.noreply.github.com>
2019-09-30 11:00:20 -0600

    Start work on 03Prove

---
+++ web/3/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/common/head.php'; ?>
+    <title><?php echo $title; ?> - Store</title>
+</head>
+<body>
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/common/scripts.php'; ?>
+</body>
+</html>
