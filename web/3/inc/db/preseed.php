6c974c3e0ce03bb9b21e169283364a449473873d
727021 <1834544+727021@users.noreply.github.com>
2019-10-01 17:07:58 -0600

    Test pg

--- web/3/inc/db/preseed.php
+++ web/3/inc/db/preseed.php
@@ -3,7 +3,8 @@
 require_once('connect.php');
 
 $queries = array(// Fill the database with preseed data
-
+    "INSERT INTO Item (name, price, description) VALUES ('Test Item', 500, 'This is a test item.')",
+    "SELECT * FROM Item"
 );
 
 $results = array();
