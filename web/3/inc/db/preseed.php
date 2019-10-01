27fdbca92efa3af044d0fe0b2420b2131c923894
727021 <1834544+727021@users.noreply.github.com>
2019-10-01 17:11:00 -0600

    Test pg

--- web/3/inc/db/preseed.php
+++ web/3/inc/db/preseed.php
@@ -3,7 +3,6 @@
 require_once('connect.php');
 
 $queries = array(// Fill the database with preseed data
-    "INSERT INTO Item (name, price, description) VALUES ('Test Item', 500, 'This is a test item.')",
     "SELECT * FROM Item"
 );
 
@@ -15,7 +14,8 @@ for ($i = 0; $result = pg_query($db, $queries[$i]); $i++) {
 pg_close($db);
 
 foreach ($results as $result) {
-    var_dump($result);
+    var_dump(pg_fetch_row($result));
+    echo '<br>';
 }
 
 ?>
