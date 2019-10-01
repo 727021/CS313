e27161c0ff751781c07a8bda802921515e75071e
727021 <1834544+727021@users.noreply.github.com>
2019-10-01 16:17:36 -0600

    Start work on store db

---
+++ web/3/inc/db/preseed.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('connect.php');
+
+$queries = array(// Fill the database with preseed data
+
+);
+
+$results = array();
+for ($i = 0; $result = pg_query($db, $queries[$i]); $i++) {
+    $results[$i] = $result;
+}
+
+pg_close($db);
+
+foreach ($results as $result) {
+    var_dump($result);
+}
+
+?>
