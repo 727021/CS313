5df6e68b346b068462e0ff9b8ae67340e36ef15b
727021 <1834544+727021@users.noreply.github.com>
2019-10-01 11:39:55 -0600

    Start work on store site

--- web/3/inc/nav.php
+++ web/3/inc/nav.php
@@ -4,14 +4,14 @@ $filename = basename($_SERVER['PHP_SELF']);
 <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container">
-            <a href="#" class="navbar-brand">CS313</a>
+            <a href="#" class="navbar-brand">Store</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item<?php if ($filename == "index.php") echo " active"; ?>"><a href="index.php" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
-                    <li class="nav-item<?php if ($filename == "cart.php") echo " active"; ?>"><a href="about.php" class="nav-link"><i class="fas fa-cart"></i> Cart</a></li>
+                    <li class="nav-item<?php if ($filename == "cart.php") echo " active"; ?>"><a href="cart.php" class="nav-link"><i class="fas fa-shopping-cart"></i> Cart</a></li>
                 </ul>
             </div>
         </div>
--- web/3/index.php
+++ web/3/index.php
@@ -1,13 +1,26 @@
-<?php echo $_SERVER['SERVER_ADDR']; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include $_SERVER['DOCUMENT_ROOT'].'/common/head.php'; ?>
+    <?php include 'inc/head.php'; ?>
     <title><?php echo $title; ?> - Store</title>
 </head>
 <body>
+    <?php include 'inc/nav.php'; ?>
 
+    <!-- Main page content -->
+    <main class="flex-shrink-0 pl-1 pr-1 pl-md-0 pr-md-0">
+        <div class="container mt-1 mb-2 p-1">
+            <div class="row"><!-- TODO Use a button and a modal to set filters -->
+                <div class="d-none d-md-block col-2"><!-- Spacer --></div>
+                <div class="col">
+                    List of items
+                </div>
+                <div class="d-none d-md-block col-2"><!-- Spacer --></div>
+            </div>
+        </div>
+    </main>
 
-    <?php include $_SERVER['DOCUMENT_ROOT'].'/common/scripts.php'; ?>
+    <?php include 'inc/top.php'; ?>
+    <?php include 'inc/scripts.php'; ?>
 </body>
 </html>
