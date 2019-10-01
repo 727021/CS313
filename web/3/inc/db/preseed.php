496abac1f97cbd572246837fa58418baad61b8b7
727021 <1834544+727021@users.noreply.github.com>
2019-10-01 17:44:56 -0600

    psql

--- web/3/inc/db/preseed.php
+++ web/3/inc/db/preseed.php
@@ -3,17 +3,6 @@
 require_once('connect.php');
 
 $queries = array(// Fill the database with preseed data
-    "INSERT INTO Array_Lookup VALUES (0,'Item','id_item')",
-    "INSERT INTO Array_Lookup VALUES (1,'Item','name')",
-    "INSERT INTO Array_Lookup VALUES (2,'Item','price')",
-    "INSERT INTO Array_Lookup VALUES (3,'Item','description')",
-    "INSERT INTO Array_Lookup VALUES (4,'Item','category')",
-    "INSERT INTO Array_Lookup VALUES (0,'Category','id_category')",
-    "INSERT INTO Array_Lookup VALUES (1,'Category','name')",
-    "INSERT INTO Array_Lookup VALUES (0,'Review','id_review')",
-    "INSERT INTO Array_Lookup VALUES (1,'Review','name')",
-    "INSERT INTO Array_Lookup VALUES (2,'Review','stars')",
-    "INSERT INTO Array_Lookup VALUES (3,'Review','id_item')"
 );
 
 $results = array();
---
+++ web/3/inc/db/query.php
@@ -0,0 +1,29 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    require_once('connect.php');
+    $query = $_POST['query']; // I know this isn't secure at all; it's temporary.
+    $result = pg_query($db, str_replace(';', '', $query));
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Andrew Schimelpfening" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Run Query</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <p>Enter a single query, without a semicolon at the end:</p>
+        <textarea name="query" id="" cols="30" rows="10" placeholder="PostgreSQL query..."></textarea>
+        <input type="submit" name="submit" value="Query">
+    </form>
+    <p>Result:</p>
+    <br>
+    <p><?php if (isset($result)) echo $result; ?></p>
+</body>
+</html>
