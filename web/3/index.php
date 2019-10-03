dd40bee1b952873aff87ff594d7d73afc018e4ee
727021 <1834544+727021@users.noreply.github.com>
2019-10-03 16:10:00 -0600

    Test

--- web/3/index.php
+++ web/3/index.php
@@ -12,7 +12,7 @@
     <main class="flex-shrink-0 pl-1 pr-1 pl-md-0 pr-md-0">
         <div class="container mt-1 mb-2 p-1">
             <div class="row"><!-- TODO Use a button and a modal to set filters -->
-                <div class="d-none d-md-block col-2"><!-- Spacer --></div>
+                <!-- <div class="d-none d-md-block col-2">Spacer</div> -->
                 <div class="col">
                     <table class="table">
                     <?php
@@ -38,7 +38,7 @@
                     ?>
                     </table>
                 </div>
-                <div class="d-none d-md-block col-2"><!-- Spacer --></div>
+                <!-- <div class="d-none d-md-block col-2">Spacer</div> -->
             </div>
         </div>
     </main>
