2698604d5a2d9b5bbc65728339c2b15bf95e504f
727021 <1834544+727021@users.noreply.github.com>
2019-10-03 15:29:20 -0600

    Actually send AJAX

--- web/3/script.js
+++ web/3/script.js
@@ -2,8 +2,11 @@ function addToCart(id) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
             var i = Number($("#itemCount").text());
             $("#itemCount").text(i + Number(this.responseText));
         }
     }
+    xhttp.open("POST", "action.php", true);
+    xhttp.send(`id=${id}`);
 }
