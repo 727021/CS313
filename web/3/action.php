eae5cd1aef4fd087d95ebd3ee7a0d8ccee807110
727021 <1834544+727021@users.noreply.github.com>
2019-10-03 15:31:17 -0600

    Test

--- web/3/action.php
+++ web/3/action.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    if (isset($_POST['id'])) {
-        array_push($_SESSION['cart'], $_POST['id']);
-        echo 1;
-    }
-    echo 0;
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    array_push($_SESSION['cart'], $_GET['id']);
+    echo 1;
 } else {
     header("location: index.php");
 }
