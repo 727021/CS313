d78847e56f7ebb89f351aef145c58065c1996b8b
727021 <1834544+727021@users.noreply.github.com>
2019-10-03 15:46:53 -0600

    Test

--- web/3/action.php
+++ web/3/action.php
@@ -2,6 +2,7 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    if (!isset($_SESSION['cart'])) { $_SESSION['cart'] = array(); }
     array_push($_SESSION['cart'], $_GET['id']);
     var_dump($_SESSION);
 } else {
