dac5806a1fc26fa60d89ed7af0c707ca31bfc720
727021 <1834544+727021@users.noreply.github.com>
2019-10-03 15:45:53 -0600

    Test

--- web/3/script.js
+++ web/3/script.js
@@ -2,11 +2,10 @@ function addToCart(id) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+            console.log(`addToCart(${id}) => ${this.responseText}`);
             var i = Number($("#itemCount").text());
             $("#itemCount").text(i + Number(this.responseText));
         }
     }
-    xhttp.open("POST", "action.php", true);
-    xhttp.send(`id=${id}`);
+    xhttp.open("GET", `action.php?id=${id}`, true);
 }
