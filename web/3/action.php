5e0c81322cf5c3245dc4e5b30f3a4b36c912620f
727021 <1834544+727021@users.noreply.github.com>
2019-10-03 15:49:35 -0600

    Test

--- web/3/action.php
+++ web/3/action.php
@@ -3,8 +3,9 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
     if (!isset($_SESSION['cart'])) { $_SESSION['cart'] = array(); }
-    array_push($_SESSION['cart'], $_GET['id']);
-    var_dump($_SESSION);
+    if (!isset($_SESSION['cart'][intval($_GET['id'])])) { $_SESSION['cart'][intval($_GET['id'])] = 1; }
+    else { $_SESSION['cart'][intval($_GET['id'])] += 1; }
+    echo 1;
 } else {
     header("location: index.php");
 }
