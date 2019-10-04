eb688bdf4eab240cc21e339ea8431fe0dc3a0457
727021 <1834544+727021@users.noreply.github.com>
2019-10-04 16:03:04 -0600

    Test

--- web/3/cart.php
+++ web/3/cart.php
@@ -23,19 +23,16 @@
                     -->
                     <?php
                         require_once('inc/db/connect.php');
-                        var_dump($_SESSION['cart']);
                         foreach ($_SESSION['cart'] as $id => $items) {
                             if (count($items > 0)) {
                                 $result = pg_fetch_all(pg_query($db, "SELECT * FROM Item WHERE id_item = $id"));
-                                var_dump($result);
-                                if ($result) {
+                                print_r($result);
                                     echo '<tr>
                                     <td>' . $result['name'] . '</td>
                                     <td>$' . number_format(floatval($result['price']) / 100.0, 2) . '</td>
                                     <td>' . count($items) . '</td>
                                     <td><button role="button" class="btn btn-danger"><i class="far fa-times-circle"></i></button></td>
                                     </tr>';
-                                }
                             }
                         }
                         pg_close($db);
