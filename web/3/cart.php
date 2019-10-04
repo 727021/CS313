37a8c37f71e826ce081f1dddbd77d42588514846
727021 <1834544+727021@users.noreply.github.com>
2019-10-04 15:40:24 -0600

    Fix cart.php

--- web/3/cart.php
+++ web/3/cart.php
@@ -26,14 +26,12 @@
                         foreach ($_SESSION['cart'] as $id => $items) {
                             if (count($items > 0)) {
                                 $result = pg_fetch_all(pg_query($db, "SELECT * FROM Item WHERE id_item = $id"));
-                                foreach ($items as $item) {
                                     echo '<tr>
-                                    <td>' . $item['name'] . '</td>
-                                    <td>$' . number_format(floatval($item['price']) / 100.0, 2) . '</td>
+                                    <td>' . $result['name'] . '</td>
+                                    <td>$' . number_format(floatval($result['price']) / 100.0, 2) . '</td>
                                     <td>' . count($items) . '</td>
                                     <td><button role="button" class="btn btn-danger><i class="far fa-times-circle"></i></button>"</td>
                                     </tr>';
-                                }
                             }
                         }
                         pg_close($db);
