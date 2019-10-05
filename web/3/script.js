6dfd46b4740be6b8a82af8e1b1d1f3366f3b0de6
727021 <1834544+727021@users.noreply.github.com>
2019-10-05 14:57:05 -0600

    Test

--- web/3/script.js
+++ web/3/script.js
@@ -20,7 +20,7 @@ function removeFromCart(id) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState = 4 && this.status == 200) {
-            console.log(`.removeItem.click(${id}) => ${this.responseText}`);
+            console.log(`.removeItem.click(${id}) => ${Number(this.responseText)}`);
             var r = Number(this.responseText);
             if (r > 0) { // Decrease count by one
                 var i = Number(btn.parent().siblings("itemCount").text());
