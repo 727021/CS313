826d8d4a71fe59aa18ff7d18dd7cd361a0cebe75
727021 <1834544+727021@users.noreply.github.com>
2019-10-15 15:25:49 -0600

    Update survey homepage

--- web/surveyproject/index.php
+++ web/surveyproject/index.php
@@ -14,15 +14,18 @@ if (isset($_SESSION['user'])) {
 </head>
 <body class="bg-info no-select">
     <div class="container">
-        <div class="vertical-center">
-            <div>
+        <div class="row">
+            <div class="d-none d-sm-block col-sm-2 col-lg-4"></div>
+            <div class="col">
                 <h1 class="display-3 text-white">Welcome!</h1>
                 <p class="text-white">It looks like you're not logged in. Click one of the buttons below to start creating your own surveys.</p>
                 <br/><br/>
                 <a href="login.php" class="btn btn-primary btn-lg" role="button">Create an Account</a>
                 <p class="text-primary m-0"><i>or</i></p>
                 <a href="login.php" class="btn btn-outline-primary">Log in</a>
+                <div class="alert alert-danger">Successfully logged out.</div>
             </div>
+            <div class="d-none d-sm-block col-sm-2 col-lg-4"></div>
         </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
