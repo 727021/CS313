b2abd6390ab52b5c74668c9523e3ac4f17f3cef3
727021 <1834544+727021@users.noreply.github.com>
2019-10-16 16:18:33 -0600

    Add shortcode table to sql

--- db/MyDB.sql
+++ db/MyDB.sql
@@ -12,48 +12,46 @@ DROP SCHEMA IF EXISTS surveys CASCADE;
 CREATE SCHEMA surveys
 
 -- This table will contain values for things like survey status and user type.
-CREATE TABLE Common_Lookup (
-	common_lookup_id SERIAL PRIMARY KEY,
-	context VARCHAR(30) NOT NULL, -- TABLE.COLUMN
-	value VARCHAR(30) NOT NULL
-)
+CREATE TABLE Common_Lookup
+( common_lookup_id SERIAL PRIMARY KEY
+, context VARCHAR(30) NOT NULL -- 'TABLE.COLUMN'
+, value VARCHAR(30) NOT NULL)
 
-CREATE TABLE Users (
-	user_id SERIAL PRIMARY KEY,
-	username VARCHAR(30) NOT NULL UNIQUE,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	first VARCHAR(30) NOT NULL,
-	middle VARCHAR(30),
-	last VARCHAR(30) NOT NULL,
-	hash VARCHAR(255) NOT NULL,-- Generated in php with hash_password()
-	type INT NOT NULL REFERENCES Common_Lookup(common_lookup_id)
-)
+CREATE TABLE Users
+( user_id SERIAL PRIMARY KEY
+, username VARCHAR(30) NOT NULL UNIQUE
+, email VARCHAR(255) NOT NULL UNIQUE
+, first VARCHAR(30) NOT NULL
+, middle VARCHAR(30)
+, last VARCHAR(30) NOT NULL
+, hash VARCHAR(255) NOT NULL -- Generated in php with hash_password()
+, type INT NOT NULL REFERENCES Common_Lookup(common_lookup_id))
 
-CREATE TABLE Survey (
-	survey_id SERIAL PRIMARY KEY,
-	title VARCHAR(30),
-	user_id INT NOT NULL REFERENCES Users(user_id),
-	status INT NOT NULL REFERENCES Common_Lookup(common_lookup_id)
-)
+CREATE TABLE survey
+( survey_id SERIAL PRIMARY KEY
+, title VARCHAR(30)
+, user_id INT NOT NULL REFERENCES users(user_id)
+, status INT NOT NULL REFERENCES common_lookup(common_lookup_id))
 
-CREATE TABLE Page (
-	page_id SERIAL PRIMARY KEY,
-	survey_id INT NOT NULL REFERENCES Survey(survey_id)
-)
+CREATE TABLE page
+( page_id SERIAL PRIMARY KEY
+, survey_id INT NOT NULL REFERENCES survey(survey_id))
 
-CREATE TABLE Question (
-	question_id SERIAL PRIMARY KEY,
-	page_id INT NOT NULL REFERENCES Page(page_id),
-	content TEXT NOT NULL
-)
+CREATE TABLE question
+( question_id SERIAL PRIMARY KEY
+, page_id INT NOT NULL REFERENCES page(page_id)
+, content TEXT NOT NULL)
 
-CREATE TABLE Response (
-	response_id SERIAL PRIMARY KEY,
-	survey_id INT NOT NULL REFERENCES Survey(survey_id),
-	ip_address VARCHAR(45),
-	response_data TEXT NOT NULL,
-	responded_on TIMESTAMP NOT NULL DEFAULT NOW()
-)
+CREATE TABLE response
+( response_id SERIAL PRIMARY KEY
+, survey_id INT NOT NULL REFERENCES survey(survey_id)
+, ip_address VARCHAR(45)
+, response_data TEXT NOT NULL
+, responded_on TIMESTAMP NOT NULL DEFAULT NOW())
+
+CREATE TABLE shortcode
+( survey_id INT NOT NULL REFERENCES survey(survey_id)
+, code VARCHAR(16) NOT NULL)
 
 -- Create a view for easy access to user info
 CREATE VIEW User_Info AS
