508b258baa8f562ed9a62625ed31eb2604cba2bf
727021 <1834544+727021@users.noreply.github.com>
2019-10-19 17:57:55 -0600

    Fix copyLink.js

--- web/surveyproject/copyLink.js
+++ web/surveyproject/copyLink.js
@@ -1,7 +1,7 @@
 $(function() {
     $("button.copyLink").each(function() {
         $(this).click(function() {
-            prompt("Share this link to your survey:", `${String(window.location).slice(0, String(window.location).lastIndexOf('/'))}/survey.php?id=${$(this).attr("data-shortcode")}`);
+            prompt("Share this link to your survey:", `${String(window.location).slice(0, String(window.location).lastIndexOf('/'))}/survey.php?s=${$(this).attr("data-shortcode")}`);
         });
     });
 });
