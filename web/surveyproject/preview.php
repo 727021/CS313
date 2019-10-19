ff40c3c8e713758ed45c977649cee9106825cd4a
727021 <1834544+727021@users.noreply.github.com>
2019-10-19 16:49:30 -0600

    Add navigation button and validation to preview

--- web/surveyproject/preview.php
+++ web/surveyproject/preview.php
@@ -30,6 +30,40 @@ $stmt2->bindValue(':sid', $sid, PDO::PARAM_INT);
 $stmt2->execute();
 $pageCount = $stmt2->fetch(PDO::FETCH_ASSOC)['count'];
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { // A page has been submitted
+    $stmt3 = $db->prepare('SELECT question_id,content FROM surveys.question WHERE page_id=(SELECT page_id FROM surveys.page WHERE page_index=:pid AND survey_id=:sid)');
+    $stmt3->bindValue(':pid', $pid - 1, PDO::PARAM_INT);
+    $stmt3->bindValue(':sid', $sid, PDO::PARAM_INT);
+    $stmt3->execute();
+
+    $required_ids = array();
+    $question_errors = array();
+
+    while ($q_validate = $stmt3->fetch(PDO::FETCH_ASSOC)) { // Collect the IDs of the required questions
+        if (json_decode($q_validate['content']->content->required)) { array_push($required_ids, $q_validate['question_id']); }
+    }
+
+    foreach ($_POST as $answer_id => $answer) {
+        if (array_search($answer_id, $required_ids) !== false) {
+            if (empty(trim($answer))) { // Collect the IDs of empty required questions
+                array_push($question_errors, $answer_id);
+            }
+        }
+    }
+
+    if (count($question_errors) == 0) {
+        if (!isset($_SESSION['response'])) {
+            $_SESSION['response'] = array();
+        }
+
+        foreach ($_POST as $answer_id => $answer) {
+            $_SESSION['response'][$answer_id] = htmlspecialchars(trim($answer));
+        }
+    } else {
+        $pid -= 1;
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,6 +77,10 @@ $pageCount = $stmt2->fetch(PDO::FETCH_ASSOC)['count'];
     <h3 class="border-bottom text-center display-4"><?php echo empty($title) ? 'Survey' : $title; ?> <small><small class="text-muted">by <?php echo $_SESSION['user']['username']; ?></small></small></h3>
     <form method="POST" action="<?php echo "preview.php?p=" . $pid + 1; ?>">
         <?php
+        if ($pid > $pageCount) {
+            echo '<p class="text-center">Thank you for your response.</p>';
+            echo '<div class="text-center"><a class="btn btn-primary" role="button" href="dashboard.php">Back to Dashboard</a></div>';
+        }  else {
             $stmt1 = $db->prepare('SELECT q.question_id AS id, q.content FROM surveys.question q, surveys.page p, surveys.survey s WHERE q.page_id = p.page_id AND s.survey_id=:sid AND p.page_index=:pid');
             $stmt1->bindValue(':sid', $sid, PDO::PARAM_INT);
             $stmt1->bindValue(':pid', $pid, PDO::PARAM_INT);
@@ -71,10 +109,11 @@ $pageCount = $stmt2->fetch(PDO::FETCH_ASSOC)['count'];
                 if ($q == null) { continue; }
 
                 // Actually display the question as HTML
-                echo $q->toHTML($qid);
+                echo $q->toHTML($qid, (array_search($qid, $question_errors) !== false));
             }
+        }
         ?>
-        <!-- TODO Add button(s) for page navigation -->
+        <input type="submit" value="<?php echo ($pid == $pageCount ? 'Submit' : 'Continue'); ?>">
     </form>
     </div>
 
