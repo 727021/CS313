8b0793a9b4f0013d1c0dc0a78a125bf63bf1c70a
727021 <1834544+727021@users.noreply.github.com>
2019-10-22 15:34:03 -0600

    Test

--- web/6/index.php
+++ web/6/index.php
@@ -7,6 +7,8 @@ try {
     $dbPass = $dbvars['pass'];
     $dbName = ltrim($dbVars['path'], '/');
 
+    var_dump($dbvars);
+
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $ex) {
