2669c13c2c7c929382929eff005331f3d20de1cb
727021 <1834544+727021@users.noreply.github.com>
2019-10-22 15:36:41 -0600

    Test

--- web/6/index.php
+++ web/6/index.php
@@ -7,7 +7,7 @@ try {
     $dbPass = $dbvars['pass'];
     $dbName = ltrim($dbVars['path'], '/');
 
-    var_dump($dbvars);
+    var_dump(getenv('DATABASE_URL'));
 
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
