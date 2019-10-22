e705c3f7961331906cb9d232f78dde2d1aec8615
727021 <1834544+727021@users.noreply.github.com>
2019-10-22 14:51:51 -0600

    Update register link on index page

--- web/surveyproject/index.php
+++ web/surveyproject/index.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['user'])) {
                         <h1 class="display-3 text-white">Welcome!</h1>
                         <p class="text-white">It looks like you're not logged in. Click one of the buttons below to start creating your own surveys.</p>
                         <br/><br/>
-                        <a href="login.php" class="btn btn-primary btn-lg" role="button">Create an Account</a>
+                        <a href="register.php" class="btn btn-primary btn-lg" role="button">Create an Account</a>
                         <p class="text-primary m-0"><i>or</i></p>
                         <a href="login.php" class="btn btn-outline-primary">Log In</a>
                     </div>
