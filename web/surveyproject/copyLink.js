00d79a1d9cf65c7085bd9d6221bb37bdd478414a
727021 <1834544+727021@users.noreply.github.com>
2019-10-25 12:54:09 -0600

    Update copyLink.js

--- web/surveyproject/copyLink.js
+++ web/surveyproject/copyLink.js
@@ -9,7 +9,7 @@ $(function() {
 
             document.execCommand("copy");
 
-            $(`[data-copy='${target}']`).fadeIn().next().fadeOut();
+            $(`[data-copy='${target}']`).fadeIn().fadeOut();
         });
     });
 });
