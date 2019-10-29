de3664d619fb8f4ecfc83f8e257e2ff527a93a1a
727021 <1834544+727021@users.noreply.github.com>
2019-10-29 14:06:43 -0600

    Vertically center save dialog

--- web/surveyproject/create.php
+++ web/surveyproject/create.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['user'])) {
         </div>
     </nav>
     <div class="modal" tabindex="-1" role="dialog" id="save-modal">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Save Survey</h5>
