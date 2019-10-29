1ff899634493a3378f20fb4c99007828ecf8f78d
727021 <1834544+727021@users.noreply.github.com>
2019-10-28 20:00:42 -0600

    Test

--- web/surveyproject/create.php
+++ web/surveyproject/create.php
@@ -108,10 +108,10 @@ if (!isset($_SESSION['user'])) {
                                         <input type="text" class="form-control question-content" value="What is your answer?" placeholder="Question">
                                     </div>
                                     <div class="question-details" style="display: none;"><!-- Not used for textbox/textarea questions -->
-                                        <div class="options" data-page="1" data-question="2">
+                                        <div class="options" data-page="1" data-question="1">
                                             <div class="row form-group option">
                                                 <div class="col-5">
-                                                    <input class="form-control" type="text" data-page="1" data-question="2" value="Choice 1">
+                                                    <input class="form-control" type="text" data-page="1" data-question="1" value="Choice 1">
                                                 </div>
                                                 <div class="col">
                                                     <button role="button" class="btn btn-danger delete-option" data-toggle="tooltip" data-placement="right" title="Delete Option"><i class="fas fa-minus"></i></button>
@@ -119,7 +119,7 @@ if (!isset($_SESSION['user'])) {
                                             </div>
                                             <div class="row form-group option">
                                                 <div class="col-5">
-                                                    <input class="form-control" type="text" data-page="1" data-question="2" value="Choice 2">
+                                                    <input class="form-control" type="text" data-page="1" data-question="1" value="Choice 2">
                                                 </div>
                                                 <div class="col">
                                                     <button role="button" class="btn btn-danger delete-option" data-toggle="tooltip" data-placement="right" title="Delete Option"><i class="fas fa-minus"></i></button>
@@ -127,7 +127,7 @@ if (!isset($_SESSION['user'])) {
                                             </div>
                                             <div class="row form-group option">
                                                 <div class="col-5">
-                                                    <input class="form-control" type="text" data-page="1" data-question="2" value="Choice 3">
+                                                    <input class="form-control" type="text" data-page="1" data-question="1" value="Choice 3">
                                                 </div>
                                                 <div class="col">
                                                     <button role="button" class="btn btn-danger delete-option" data-toggle="tooltip" data-placement="right" title="Delete Option"><i class="fas fa-minus"></i></button>
