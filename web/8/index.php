de57c032ecfffdf8d6c53e8376c66854b06d5097
727021 <1834544+727021@users.noreply.github.com>
2019-11-05 11:02:52 -0700

    Prove 08 page

--- web/8/index.php
+++ web/8/index.php
@@ -32,7 +32,10 @@
 
     <div class="container mt-2 mb-2">
         <div class="code-highlight js border rounded p-2">
-            <h3 class="border-bottom text-muted">index.js</h3>
+            <h3 class="border-bottom text-muted display-4">
+                Prove 08
+                <small><small><a class="text-muted" target="_blank" href="https://github.com/727021/CS313/blob/master/web/8/index.js">index.js</a></small></small>
+            </h3>
             <?php
                 echo nl2br(str_replace(' ', '&nbsp;', htmlspecialchars(file_get_contents('index.js'))));
             ?>
