4c3c9ba91be52b78ddf24e29a5717cdd2093a788
727021 <1834544+727021@users.noreply.github.com>
2019-11-05 10:54:03 -0700

    Test

--- web/8/index.php
+++ web/8/index.php
@@ -11,9 +11,28 @@
     <link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.16.2/build/styles/default.min.css">
   </head>
   <body>
+  <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">CS313</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a href="../2/index.php" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
+                        <li class="nav-item"><a href="../2/about.php" class="nav-link"><i class="fas fa-question"></i> About</a></li>
+                        <li class="nav-item"><a href="../2/assignments.php" class="nav-link"><i class="fas fa-list-ul"></i> Assignments</a></li>
+                        <li class="nav-item"><a class="nav-link" target="_blank" href="https://727021.github.io"><i class="fab fa-github"></i> Github</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-    <div class="container">
+    <div class="container mt-2 mb-2">
         <div class="code-highlight js border rounded p-2">
+            <h3 class="border-bottom text-muted">index.js</h3>
             <?php
                 echo nl2br(str_replace(' ', '&nbsp;', htmlspecialchars(file_get_contents('index.js'))));
             ?>
