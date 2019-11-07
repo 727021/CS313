55019eefb02286e3565bf8815cef74b2a575d6fa
727021 <1834544+727021@users.noreply.github.com>
2019-11-07 15:26:53 -0700

    Class 08

--- web/8/class/index.js
+++ web/8/class/index.js
@@ -1,6 +1,11 @@
 const http = require('http')
+const fs = require('fs')
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'})
-    res.end('Hello World!')
-}).listen(8080)
+    try {
+        fs.createReadStream(process.argv[3]).pipe(res)
+    } catch {
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.end('Page Not Found')
+    }
+}).listen(process.argv[2] || 8080)
